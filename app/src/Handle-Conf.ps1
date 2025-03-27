feb80c144dbb638d02d96b0a8b6953265106bf71
Norzolrat <56172821+Norzolrat@users.noreply.github.com>
2025-03-27 11:06:05 +0000

    ajout de la fonction

--- app/src/Handle-Conf.ps1
+++ app/src/Handle-Conf.ps1
@@ -38,7 +38,7 @@ function Get-RevisionSelector {
             $($revisionOptions -join "`n")
         </select>
         <button onclick='showRevision(this)' style='padding: 5px 10px; margin-right: 10px;'>Voir le contenu</button>
-        <button onclick='showRevision(this)' style='padding: 5px 10px; margin-right: 10px;'>Voir les différences</button>
+        <button onclick='diffRevision(this)' style='padding: 5px 10px; margin-right: 10px;'>Voir les différences</button>
     </div>
     <div id='revisionContent' style='display: none; margin-top: 15px;'>
         <h3 id='revisionTitle' style='margin-bottom: 10px;'></h3>
@@ -61,6 +61,14 @@ function showRevision(button) {
     } else {
         window.location.href = url + "?"+parentid+"="+rev;
     }
+}
+
+function diffRevision(button) {
+    let rev_sel = button.closest('.revision-section');
+    let parentid = rev_sel.parentElement.id;
+    let rev = rev_sel.querySelector('#revisionSelect').value;
+    if (!rev) return;
+    window.location.href = "/diff?"+parentid"="+rev;
 }
 
 function filterConfigs() {
